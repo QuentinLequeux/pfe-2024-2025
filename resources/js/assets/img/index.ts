fe83709fa004bed9fd2b362e7b8e7212531a9950
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-03-21 10:10:53 +0100

    Add webp images

--- resources/js/assets/img/index.ts
+++ resources/js/assets/img/index.ts
@@ -3,11 +3,11 @@
 import animals from './animals.webp';
 import catanddog from './cat-dog.webp';
 import charleroi from './charleroi.webp';
-import croixbleue from './croixbleue.png';
+import croixbleue from './croixbleue.webp';
 import dashboard from './dashboard.png';
 import lalouviere from './lalouviere.webp';
-import mouscron from './mouscron.png';
-import sanscollier from './sanscollier.png';
-import veeweyde from './veeweyde.png';
+import mouscron from './mouscron.webp';
+import sanscollier from './sanscollier.webp';
+import veeweyde from './veeweyde.webp';
 
 export { animals, catanddog, charleroi, croixbleue, dashboard, lalouviere, mouscron, sanscollier, veeweyde };
