319e66452432fcd71b488486e1efa00077404e3a
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-03-23 20:25:03 +0100

    Update : refactoring

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Animals;
 use App\Models\Breeds;
+use App\Models\Organizations;
 use App\Models\Species;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -25,5 +26,7 @@ class DatabaseSeeder extends Seeder
         Breeds::factory(5)->create(); // Génère 5 races aléatoires.
 
         Animals::factory(10)->create(); // Génère 10 animaux aléatoires.
+
+        Organizations::factory(5)->create(); // Génère 5 refuges aléatoires.
     }
 }
--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,44 +1,9 @@
 import { Button } from '@/components/ui/button';
 import { Head, Link } from '@inertiajs/react';
-import React, { useEffect, useState } from 'react';
 import { animals, catanddog, charleroi, croixbleue, dashboard, lalouviere, mouscron, sanscollier, veeweyde } from '../assets/img';
 import SearchAnimals from '@/components/petshelter/search-animals';
-
-function ScrollButton() {
-    const [isVisible, setIsVisible] = useState(false);
-    const viewportHeight = 600;
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > viewportHeight) {
-                setIsVisible(true);
-            } else {
-                setIsVisible(false);
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
-
-    const scrollToTop = () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
-
-    return (
-        <div>
-            <Button onClick={scrollToTop} className={`bg-main hover:bg-hover !p-2.5 fixed right-5 bottom-5 rounded-full shadow-xl transition-opacity duration-500 ${isVisible ? 'opacity-100' : 'pointer-events-none opacity-0'}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                    <path d="M1 8L8 1M8 1L15 8M8 1V15" stroke="#09090B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                </svg>
-            </Button>
-        </div>
-    );
-}
-
-{
-    /* Quid du composant ? */
-}
+import ScrollButton from '@/components/petshelter/scroll-button';
+import React from 'react';
 
 export default function Welcome() {
     return (
