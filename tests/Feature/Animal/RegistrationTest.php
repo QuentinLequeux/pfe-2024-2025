a97142f898f17eec93dfa6e058d4554eee8570b9
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-04-25 16:49:28 +0200

    Test about animal registration

---
+++ tests/Feature/Animal/RegistrationTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Breeds;
+use App\Models\Organizations;
+use App\Models\Species;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+test('animal can be added', function () {
+    $organization = Organizations::factory()->create();
+    Species::factory()->create();
+    $breed = Breeds::factory()->create();
+    $user = User::factory()->create([
+        'organization_id' => $organization->id,
+    ]);
+
+    $this->actingAs($user);
+
+    $response = $this->post('/animals/create', [
+        'name' => 'test',
+        'age' => '10',
+        'weight' => '33',
+        'description' => '',
+        'arrival_date' => '2025-04-25',
+        'gender' => 'Mâle',
+        'adoption_status' => 'Disponible',
+        'breed_id' => $breed->id,
+        'organization_id' => $organization->id,
+    ]);
+
+    $this->assertAuthenticated();
+    $response->assertRedirect(route('animals', absolute: false));
+});
