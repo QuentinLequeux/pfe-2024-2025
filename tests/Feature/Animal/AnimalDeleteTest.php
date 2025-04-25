3dbf6951b9071da3868d08b40b9790f327f75fc7
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-04-25 22:39:38 +0200

    Test about animal deletion

---
+++ tests/Feature/Animal/AnimalDeleteTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Animals;
+use App\Models\Breeds;
+use App\Models\Organizations;
+use App\Models\Species;
+use App\Models\User;
+
+test('animal can be deleted', function () {
+    $organization = Organizations::factory()->create();
+    Species::factory()->create();
+    $breed = Breeds::factory()->create();
+    $user = User::factory()->create([
+        'organization_id' => $organization->id,
+    ]);
+
+    $this->actingAs($user);
+
+    $animal = Animals::factory()->create([
+        'organization_id' => $organization->id,
+        'breed_id' => $breed->id,
+    ]);
+
+    $response = $this->delete("/animals/$animal->id", [
+        'name' => 'test',
+        'age' => '10',
+        'weight' => '33',
+        'description' => '',
+        'arrival_date' => '2025-04-25',
+        'gender' => 'Mâle',
+        'adoption_status' => 'Disponible',
+        'breed_id' => $breed->id,
+        'organization_id' => $organization->id,
+    ]);
+
+    $this->assertAuthenticated();
+    $response->assertRedirect(route('animals', absolute: false));
+    $response->assertStatus(302);
+});
