1ca85e2c0ca0c2cadb28e7325f2da4edccd1159a
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-05-03 14:13:38 +0200

    Add Trait for store and resize image

---
+++ app/Concerns/HandleImageUpload.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Concerns;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
+use Intervention\Image\Laravel\Facades\Image;
+
+trait HandleImageUpload
+{
+    /**
+     * Store and resize an uploaded image.
+     *
+     * @param UploadedFile $file
+     * @param  int  $width
+     * @param  int  $height
+     * @return string
+     */
+    public function storeAndResizeImage(UploadedFile $file, int $width = 400, int $height = 300): string
+    {
+        $path = $file->store( 'public');
+
+        $imageResized = Image::read($file)->cover($width, $height);
+
+        $imageResized->save(storage_path('app/' . $path));
+
+        return Str::afterLast($path, '/');
+    }
+}
