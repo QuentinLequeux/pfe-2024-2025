bb95f823a92e41e2f5ab4d4bbb1198c3cef17aab
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-05-06 15:48:01 +0200

    Add DonationController

---
+++ app/Http/Controllers/DonationController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Stripe\Checkout\Session;
+use Stripe\Exception\ApiErrorException;
+use Stripe\Stripe;
+
+class DonationController extends Controller
+{
+    /**
+     * @throws ApiErrorException
+     */
+    public function process(Request $request)
+    {
+        $request->validate([
+            'amount' => 'required|numeric|min:1|decimal:0,2',
+        ]);
+
+        Stripe::setApiKey(config('services.stripe.secret'));
+
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [
+                [
+                    'price_data' => [
+                        'currency' => 'eur',
+                        'product_data' => [
+                            'name' => 'Don',
+                        ],
+                        'unit_amount' => $request->amount * 100,
+                    ],
+                    'quantity' => 1,
+                ],
+            ],
+            'mode' => 'payment',
+            'success_url' => route('animals'),
+            'cancel_url' => route('animals'),
+        ]);
+
+        return Inertia::location($session->url);
+    }
+}
