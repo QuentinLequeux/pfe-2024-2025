1e1f4f63fb33564797f72292ef4a0934abe36931
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-05-29 15:03:07 +0200

    Add sponsorship migration

---
+++ database/migrations/2025_05_29_084135_create_sponsorships_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sponsorships', function (Blueprint $table) {
+            $table->id();
+            $table->string('stripe_session_id')->unique();
+            $table->foreignId('user_id');
+            $table->foreignId('animal_id');
+            $table->integer('amount');
+            $table->dateTime('sponsored_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sponsorships');
+    }
+};
