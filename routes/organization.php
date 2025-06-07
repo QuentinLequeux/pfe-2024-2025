f361d33b6fdc14fcbfe6c13b52972a4c2095c4e7
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-06-07 15:44:25 +0200

    Add new organization routes

--- routes/organization.php
+++ routes/organization.php
@@ -10,9 +10,11 @@ Route::middleware(['auth'])->group(function () {
         ]);
     })->name('organization.show');
 
-    Route::get('/organizations/create', function () {
-       return Inertia::render('organization/create');
-    })->name('organization.create');
+    Route::get('/organization/create', [OrganizationController::class, 'create'])
+        ->name('organization.create');
+
+    Route::post('/organization/create', [OrganizationController::class, 'store'])
+        ->name('organization.store');
 
     Route::get('/organization/admin', [OrganizationController::class, 'show'])
         ->name('organization.admin');
