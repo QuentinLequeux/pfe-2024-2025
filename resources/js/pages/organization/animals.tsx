369d3de72b4729cf7cd8b0446a57cc01c8a228f4
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-06-07 22:23:10 +0200

    Add page for animals by organization

---
+++ resources/js/pages/organization/animals.tsx
@@ -0,0 +1,26 @@
+import { Head } from '@inertiajs/react';
+import { BreadcrumbItem } from '@/types';
+import AppLayout from '@/layouts/app-layout';
+import Card from '@/components/petshelter/card';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Animaux',
+        href: '#',
+    }
+];
+
+export default function byOrganization({ organization }: { organization: { name: string } })
+{
+    return(
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title={`Animaux - ${organization.name}`} />
+            <div className={'p-6'}>
+                <h2 aria-level={2} role={'heading'} className={'font-bold text-2xl mb-2'}>
+                    Animaux - {organization.name}
+                </h2>
+                <Card/>
+            </div>
+        </AppLayout>
+    );
+}
