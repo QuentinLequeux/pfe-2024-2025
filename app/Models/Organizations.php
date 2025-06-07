ddd2f770f0b9306939da40173902a17f5e8f2fd7
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-06-07 16:29:06 +0200

    Update Organization Model

--- app/Models/Organizations.php
+++ app/Models/Organizations.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Organizations extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'phone',
+        'email',
+        'iban',
+        'website',
+    ];
 }
