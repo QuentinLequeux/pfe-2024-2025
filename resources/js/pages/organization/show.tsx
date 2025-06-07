2e69b4ee276f3b58cc11f931d9f5eaeccde7fea4
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-06-07 20:04:56 +0200

    Setting up organization show

--- resources/js/pages/organization/show.tsx
+++ resources/js/pages/organization/show.tsx
@@ -3,8 +3,10 @@ import { useEffect } from 'react';
 import { BreadcrumbItem } from '@/types';
 import AppLayout from '@/layouts/app-layout';
 import { Button } from '@/components/ui/button';
+import { IOrganization } from '@/types/IOrganization';
 import { Head, Link, usePage } from '@inertiajs/react';
 import { PageProps as InertiaPageProps } from '@inertiajs/core';
+import { AtSign, Building, Landmark, Mail, Phone } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -15,6 +17,7 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 interface PageProps extends InertiaPageProps {
     success?: string;
+    organizations: IOrganization[];
 }
 
 export default function Organizations() {
@@ -41,6 +44,26 @@ export default function Organizations() {
                     </Link>
                 </Button>
             </div>
+            <div className={'w-full flex flex-wrap gap-4 justify-center my-4'}>
+                {props.organizations.length === 0 && <p>Aucune organisation</p>}
+                {props.organizations.map(organization => (
+                    <div key={organization.id} className={'border rounded-2xl p-6 flex flex-col gap-4 w-[30%] min-w-[400px] shadow-md'}>
+                        <p className={'font-bold'}>{organization.name}</p>
+                        <p><Building className={'inline mr-2'}/>{organization.address}</p>
+                        <a className={'underline'} href={`tel:${organization.phone}`} title={'Appeler ce numéro'}><Phone className={'inline mr-2'}/>{organization.phone}</a>
+                        <a className={'underline'} href={`mailto:${organization.email}`} title={'Envoyer un email'}><Mail className={'inline mr-2'}/>{organization.email}</a>
+                        <a className={'underline'} href={organization.website} title={`Vers ${organization.website}`} target={'_blank'}><AtSign className={'inline mr-2'}/>{organization.website}</a>
+                        <p><Landmark className={'inline mr-2'}/>{organization.iban}</p>
+                        <Button asChild className={'bg-main hover:bg-hover font-bold text-black w-fit m-auto'}>
+                            <Link href={'#'}>
+                                Voir les animaux
+                            </Link>
+                        </Button>
+                    </div>
+                ))}
+            </div>
         </AppLayout>
     );
 }
+
+// TODO : Voir les animaux du refuge.
