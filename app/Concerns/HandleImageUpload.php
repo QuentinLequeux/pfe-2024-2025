a46b30085348f608b09a7b038287fbfd3945a44a
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-06-08 11:38:50 +0200

    Update : refactoring

--- app/Concerns/HandleImageUpload.php
+++ app/Concerns/HandleImageUpload.php
@@ -19,18 +19,22 @@ trait HandleImageUpload
      */
     public function storeAndResizeImage(UploadedFile $file, int $width = 400, int $height = 300): string
     {
-        $path = $file->store( '', 's3');
+        //$path = $file->store( '', 's3');
+
+        $fileName = uniqid() . '.' . $file->getClientOriginalExtension();
 
         $imageResized = Image::read($file)->cover($width, $height);
 
         $imageData = (string) $imageResized->encode();
 
-        $fileName = basename($path);
+        //$fileName = basename($path);
 
-        Storage::disk('s3')->put($fileName, $imageData, ['visibility' => 'public']);
+        Storage::disk('s3')->put($fileName, $imageData, 'public');
 
         //$imageResized->save(Storage::disk('s3')->path(basename($path)));
 
-        return Str::afterLast($path, '/');
+        $publicBase = 'https://petshelter.367be3a2035528943240074d0096e0cd.r2.dev';
+
+        return "{$publicBase}/{$fileName}";
     }
 }
