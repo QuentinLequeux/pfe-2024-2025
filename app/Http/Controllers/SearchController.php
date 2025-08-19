74b4b9868e6429f65652ba5e70eb10348dceb907
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-08-19 15:47:14 +0200

    Update : refactor SearchController for filters

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Animal;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
 use Illuminate\Support\Facades\Storage;
 
 class SearchController extends Controller
@@ -14,12 +16,25 @@ class SearchController extends Controller
         $limit = $request->get('limit');
 
         if ($query) {
-            $animalsQuery = Animal::search($query);
-            $animals = $limit ? $animalsQuery->take((int) $limit)->get() : $animalsQuery->get();
-            $animals->load('breed');
+            $id = Animal::search($query)->get()->pluck('id');
+            $animals = QueryBuilder::for(Animal::with('breed')->whereIn('id', $id))
+                ->allowedFilters([
+                    AllowedFilter::exact('gender')
+                ])
+                ->allowedSorts(['name'])
+                ->when($limit, fn ($q) => $q->limit($limit))
+                ->get();
         } else {
-            $animals = Animal::with('breed')->inRandomOrder();
-            $animals = $limit ? $animals->limit((int) $limit)->get() : $animals->get();
+            $animals = QueryBuilder::for(Animal::with('breed'))
+                ->allowedFilters([
+                    AllowedFilter::exact('gender'),
+                    AllowedFilter::exact('adoption_status'),
+                    AllowedFilter::exact('breed_id'),
+                    AllowedFilter::exact('organization_id'),
+                    AllowedFilter::exact('breed.specie_id'),
+                ])
+                ->allowedSorts(['name'])
+                ->when($limit, fn($q) => $q->limit((int) $limit))->get();
         }
 
         $animals->each(function ($animal) {
