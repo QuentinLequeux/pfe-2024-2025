ff2b82f2f7fdb31e61984429aa6077d1f9b6718e
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-08-29 23:36:10 +0200

    Add button to transactions

--- resources/js/pages/sponsorship/sponsorship.tsx
+++ resources/js/pages/sponsorship/sponsorship.tsx
@@ -1,7 +1,8 @@
-import { Head } from '@inertiajs/react';
 import { BreadcrumbItem } from '@/types';
 import AppLayout from '@/layouts/app-layout';
+import { Head, Link } from '@inertiajs/react';
 import Card from '@/components/petshelter/card';
+import { Button } from '@/components/ui/button';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -15,7 +16,14 @@ export default function Sponsorship() {
     return(
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title={'Parrainage'} />
-            <div className={'p-6 h-full'}>
+            <div className={'p-6 w-full flex justify-end'}>
+                <Button title={'Historique des dons'} className={'bg-main hover:bg-hover text-black font-bold'} asChild>
+                    <Link href={route('history')}>
+                        Historique des dons
+                    </Link>
+                </Button>
+            </div>
+            <div className={'p-6'}>
                 <Card/>
             </div>
         </AppLayout>
